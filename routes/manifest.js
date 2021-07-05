40cd8f0ee2f53503baafcd9fc7689807fb42a35c
ken <ken.trinh@ymail.com>
2021-07-05 14:00:02 +1000

    new

--- routes/manifest.js
+++ routes/manifest.js
@@ -56,6 +56,40 @@ router.post('/', function(req, res) {
                 if (err) return console.log('Error loading client secret file:', err);
                 // Authorize a client with credentials, then call the Google Sheets API.
                 authorize(JSON.parse(content), listMajors)
+                request.post({
+                    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                    url: 'https://iam.ng.bluemix.net/identity/token',
+                    body: api_token
+                }, function(error, response, body) {
+                    token = JSON.parse(body)
+                    // console.log(token.access_token);
+                    var Authorization = "Bearer " + token.access_token
+
+                    j_data = JSON.stringify({
+                        "job_run": {
+                            "configuration": {
+
+                            }
+                        }
+                    })
+
+                    request.post({
+                        headers: { "Content-Type": "application/json", "Authorization": Authorization },
+                        url: s_url,
+                        body: j_data
+                    }, function(error, response, body) {
+
+                        // res.send('A job is run')
+                        // console.log(body)
+                        message = JSON.parse(body)
+                        console.log(message.metadata)
+                        res.send(200, res, message)
+                        return response
+
+                    });
+
+                });
+
 
             })
 
@@ -163,39 +197,6 @@ router.post('/', function(req, res) {
 
                         })
 
-                        request.post({
-                            headers: { 'content-type': 'application/x-www-form-urlencoded' },
-                            url: 'https://iam.ng.bluemix.net/identity/token',
-                            body: api_token
-                        }, function(error, response, body) {
-                            token = JSON.parse(body)
-                            // console.log(token.access_token);
-                            var Authorization = "Bearer " + token.access_token
-
-                            j_data = JSON.stringify({
-                                "job_run": {
-                                    "configuration": {
-
-                                    }
-                                }
-                            })
-
-                            request.post({
-                                headers: { "Content-Type": "application/json", "Authorization": Authorization },
-                                url: s_url,
-                                body: j_data
-                            }, function(error, response, body) {
-
-                                // res.send('A job is run')
-                                // console.log(body)
-                                message = JSON.parse(body)
-                                console.log(message.metadata)
-                                res.send(200, res, message)
-                                return response
-
-                            });
-
-                        });
 
                         // setTimeout(function() {;
 
