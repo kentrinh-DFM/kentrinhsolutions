ca1da0eaafbb922c1abf6c2b1081297dd3d098f3
kenTrinh <ken.trinh@ymail.com>
2022-03-31 01:10:10 +1000

    new change

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,14 @@
 var app = angular.module('myApp', []);
+
+
+app.config(function($sceDelegateProvider){
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    "http://www.youtube.com/embed/**"
+  ]);
+})
+
+
 app.controller('myCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
     $scope.selected = "about";
 
@@ -7,13 +17,40 @@ app.controller('myCtrl', ['$scope', '$http', '$window', function($scope, $http, 
         id: 'about'
     },
     {
-        id: 'porfolio'
+        id: 'portfolio'
     }
+    ]
+
+     $scope.works = [
+    {
+        title: 'Budget Scenario Planning',
+        src: '//www.youtube.com/embed/dpdc1nmORfg',
+        text: 'Indicative Forecasting for budget planning.'
+    },
+    {
+        title: 'GIS Scenario Planning',
+        src: '//www.youtube.com/embed/KfigiNUj694',
+        text: 'Scenario Facilities Planning using Geospatial Technology.'
+    },
+    {
+        title: 'PowerBI Custom Visualisation',
+        src: '//www.youtube.com/embed/lJAmwsBY14Q',
+        text: 'Cavity Filling Analysis using PowerBI heatmap custom visualisation.'
+    },
+    {
+        title: 'Tableau Services Analytics Dashboard',
+        src: '//www.youtube.com/embed/R3mth4vsl44',
+        text: 'End-to-end services analysis reporting.'
+    }
+
     ]
     
     $scope.go = function(item){
         $scope.selected = item.id
     }
+
+
+
 
 
 }])
